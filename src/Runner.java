d7a28170543316e5814719036d7c6642a6ae85b9
TysonYung <sample@bths.edu>
2025-11-20 10:08:18 -0500

    modified print function

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Runner {
-    public void print(){
-
+    public void print(String msg, boolean newLine) {
+        System.out.println(msg + ((newLine) ? "\n" : ""));
     }
 
     public static void main(String[] args) {
