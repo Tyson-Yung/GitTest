227cfa5fbaa33a0fdf0a68018875dd3970924774
TysonYung <sample@bths.edu>
2025-11-20 10:13:23 -0500

    added Funtimes.java modified main method in Runner.java modified main method in Funtimes.java

---
+++ src/Funtimes.java
@@ -0,0 +1,5 @@
+public class Funtimes {
+    public static void main(String[] args) {
+        Runner.print("WOW!", true);
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Runner {
-    public void print(String msg, boolean newLine) {
+    public static void print(String msg, boolean newLine) {
         System.out.println(msg + ((newLine) ? "\n" : ""));
     }
 
     public static void main(String[] args) {
        System.out.println("Hello world");
+       print("HI", false);
     }
 }
